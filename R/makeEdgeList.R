138fda3396ce860991661c2498e7d7f382ff4ce5
Zucca <c.zucca@tue.nl>
2020-09-24 11:52:40 +0200

    some documentation improvements

--- R/makeEdgeList.R
+++ R/makeEdgeList.R
@@ -33,8 +33,11 @@
 #'
 #' @examples
 #' \dontrun{
-#' namedf <- data.frame(respondent = c('A', 'B', 'C'), alter1 = c('D', 'C', 'A'), alter2 = c('B', 'A', 'A') )
-#' attributedf <- data.frame(attribute1 = 1:3, attribute2 = 4:6, attribute3 = 2:4)
+#' namedf <- data.frame(respondent = c('A', 'B', 'C'), 
+#'                       alter1 = c('D', 'C', 'A'), alter2 = c('B', 'A', 'A') )
+#'                       attributedf <- data.frame(attribute1 = 1:3, 
+#'                       attribute2 = 4:6, 
+#'                       attribute3 = 2:4)
 #' 
 #' edgelist <-  makeEdgeList(names =  namedf, attribute = attributedf)  
 #' }
