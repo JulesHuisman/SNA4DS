1ccd7799343bf23823dbfcbaad02f699b33c7b57
SNAnalyst <r.t.a.j.leenders@tilburguniversity.edu>
2020-11-30 12:59:04 +0100

    "sender", "receiver", "equal" added

--- R/make_matrix_from_vertex_attribute.R
+++ R/make_matrix_from_vertex_attribute.R
@@ -23,7 +23,21 @@
 #' \item max the highest value between the two vertices
 #' \item min the highest value between the two vertices
 #' \item mean the mean between the two vertices
+#' \item sender the value of the sender's attribute in the entire row
+#' \item receiver the value of the receiver's attribute in the entire column
+#' \item equal 1 if both vertices have the same on the attribute, 0 otherwise
 #' }
+#'
+#' Most of the options speak for themselves.
+#' The \code{sender} option is used to model a sender effect. For example,
+#' if it is to be tested whether the sender's age drives edges,
+#' the sender's age is the edge attribute for every edge this sender is
+#' involved in. Mathematically, this means that the sender's age
+#' should be in all of the cells of that sender's row.
+#'
+#' Similarly, the \code{receiver} option places the receiver's attribute in
+#' each column. For the attribute 'age', this makes the edge attribute equal
+#' to the receiver's age, for every edge the vertex is the receiver.
 #'
 #' If \code{diag == FALSE} (the default), the diagonal of the returned matrix
 #' will contain zeroes. If \code{diag} is any other value, then that will be
@@ -32,7 +46,7 @@
 #' If \code{diag == TRUE}, the \code{measure} will also be applied to the
 #' diagonal elements.
 #'
-#' @param x input, a numeric vector or an object of class \code{igraph} or
+#' @param x input, a vector or an object of class \code{igraph} or
 #' \code{network}
 #' @param name the name of the attribute to be extracted. This will only be used
 #' if \code{x} is of class \code{igraph} or \code{network}
@@ -50,6 +64,8 @@
 #' make_matrix_from_vertex_attribute(1:5, measure = "max", diag = NA)
 #' make_matrix_from_vertex_attribute(1:5, measure = "sum", diag = TRUE)
 #' make_matrix_from_vertex_attribute(1:5, measure = "min", diag = 11:15)
+#' make_matrix_from_vertex_attribute(1:5, measure = "sender")
+#' make_matrix_from_vertex_attribute(1:5, measure = "receiver")
 #' \dontrun{
 #' data(florentine, package = "SNA4DS")
 #' # absdiff, with zeroes on the diagonal
@@ -59,19 +75,22 @@
 make_matrix_from_vertex_attribute <- function (x, name,
                                                 measure = c("absdiff", "diff",
                                                             "sum", "max", "min",
-                                                            "mean"),
+                                                            "mean", "sender",
+                                                            "receiver",
+                                                            "equal"),
                                                 diag = FALSE) {
+
+  kall <- as.list(match.call())
+
   if (inherits(x, "igraph")) {
     X <- igraph::get.vertex.attribute(graph = x, name = name)
   } else if (inherits(x, "network")) {
-      X <- network::get.vertex.attribute(flomar_network, name)
+      X <- network::get.vertex.attribute(x, name)
   } else if (is.null(dim(x)) || max(dim(x)) == 1) {
       X <- x
   } else (
-    stop ("'x' has to be an igraph object, a network object, or a numeric vector")
+    stop ("'x' has to be an igraph object, a network object, or a vector")
   )
-
-  if (!is.numeric(X)) stop("The data should be numeric only.")
   if (any(is.na(X))) stop("The attribute contains missing data, which can not
                           logically be handled by this function.")
 
@@ -83,17 +102,29 @@ make_matrix_from_vertex_attribute <- function (x, name,
 
   XY <- cbind(X, Y)
   if (FUN == "diff") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- X - Y
   } else if (FUN == "absdiff") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- abs(X - Y)
   } else if (FUN == "sum") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- X + Y
   } else if (FUN == "max") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- apply(XY, 1, max)
   } else if (FUN == "min") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- apply(XY, 1, min)
   } else if (FUN == "mean") {
+    if (!is.numeric(X)) stop("The data should be numeric for this measure.")
     robj <- apply(XY, 1, mean)
+  } else if (FUN == "sender") {
+    robj <- X
+  } else if (FUN == "receiver") {
+    robj <- Y
+  } else if (FUN == "equal") {
+    robj <- (X == Y)
   }
 
   dim(robj) <- c(dX, dX)
